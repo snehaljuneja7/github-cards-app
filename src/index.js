01d0def01785c563a7e3b49af2c401364a15ef08
Snehal Juneja <shikhasnehaljuneja@gmail.com>
2021-07-10 12:56:51 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
+import React, { useState} from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Card(props) {
+  //const profile = props.profile;
+  return(
+    <div className="github-profile">
+      <img src={props.avatar_url} alt="GitHub-Avatar" />
+      <div className="info">
+        <div className="name">{props.name}</div>
+        <div className="company">{props.company}</div>
+      </div>
+    </div>
+  );  
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function CardList(props) {
+  return(
+    <div>
+      {props.profiles.map(profile => <Card name={profile.name} company={profile.company} avatar_url={profile.avatar_url} />)}
+    </div>
+  );
+}
+
+function Form(props) {
+  const [userName, setUserName] = useState('');
+  const [err, setErr] = useState(null);
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await axios.get(`https://api.github.com/users/${userName}`);
+      props.onSubmit(response.data);
+      setUserName('');
+      setErr(null);
+    } catch(err) {
+      setErr(err);
+      setUserName('');
+    }
+  }
+  const message = ' Please Make Sure You Enter A Valid Username';
+    return(
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="GitHub Username" value={userName} onChange={(e) => setUserName(e.target.value)} required></input>
+        <button>Add Card</button>
+        <br />
+        {err &&(<span className="error"><br/>{message}</span>)}
+      </form>
+    );
+  
+}
+
+function App(props) {
+  const [profiles, setProfiles] = useState([]);
+  function handleSubmit(newProfile) {
+    setProfiles(profiles.concat(newProfile));
+  };
+  return (
+    <div>
+      <div className="header">{props.title}</div>
+      <Form onSubmit={handleSubmit}/>
+      <CardList profiles = {profiles} />
+    </div>
+  );
+}
+
+ReactDOM.render(<App title="The GitHub Cards App" />, document.getElementById('root'));
